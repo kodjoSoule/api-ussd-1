d15aff0dd28b156dec7250c584d8694069b09252
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-05-30 10:48:53 +0000

    Refactor USSD service to use English language and update messages

--- index.js
+++ index.js
@@ -4,41 +4,36 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// Configuration du middleware pour analyser les requêtes entrantes
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Définir une route POST pour gérer les requêtes USSD
 app.post("/ussd", (req, res) => {
 	const { sessionId, serviceCode, phoneNumber, text } = req.body;
 
 	let response = "";
 
-	// Logique pour les différentes étapes du service USSD
 	if (text === "") {
-		response = "Bienvenue au Service Démo \n";
-		response += "1. Mon compte \n";
-		response += "2. Mon numéro de téléphone";
+		response = "Welcome To Demo Service \n";
+		response += "1. My account \n";
+		response += "2. My phone number";
 	} else if (text === "1") {
-		response = "CON Choisissez l'information que vous voulez consulter \n";
-		response += "1. Solde du compte \n";
-		response += "2. Numéro du compte";
+		response = "CON Choose account information you want to view \n";
+		response += "1. Account balance \n";
+		response += "2. Account number";
 	} else if (text === "2") {
-		response = `END Votre numéro de téléphone est ${phoneNumber}`;
+		response = `END Your phone number is ${phoneNumber}`;
 	} else if (text === "1*1") {
-		response = "END Le solde de votre compte est de 10.000 FCFA";
+		response = "END Your account balance is KES 10.000 FCFA";
 	} else if (text === "1*2") {
-		response = "END Votre numéro de compte est ACC1001";
+		response = "END Your account number is ACC1001";
 	} else {
-		response = "END Entrée invalide";
+		response = "END Invalid input";
 	}
 
-	// Définir le type de contenu et envoyer la réponse
 	res.set("Content-Type", "text/plain");
 	res.send(response);
 });
 
-// Lancer le serveur et écouter les requêtes sur le port 3000
 app.listen(port, () => {
-	console.log(`L'application USSD écoute sur http://localhost:${port}`);
+	console.log(`USSD app listening at http://localhost:${port}`);
 });
