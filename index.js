f79a6e4d0301968608ccb7a472c0cbf9f7a48cae
Souleymane KODJO <76671691+kodjoSoule@users.noreply.github.com>
2024-05-30 10:36:54 +0000

    chore: Update welcome message and remove unnecessary comments

--- index.js
+++ index.js
@@ -13,23 +13,18 @@ app.post("/ussd", (req, res) => {
 	let response = "";
 
 	if (text === "") {
-		// This is the first request. Note how we start the response with CON
-		response = "CON What would you like to check \n";
+		response = "Welcome To Demo Service \n";
 		response += "1. My account \n";
 		response += "2. My phone number";
 	} else if (text === "1") {
-		// Business logic for first level response
 		response = "CON Choose account information you want to view \n";
 		response += "1. Account balance \n";
 		response += "2. Account number";
 	} else if (text === "2") {
-		// Business logic for first level response
 		response = `END Your phone number is ${phoneNumber}`;
 	} else if (text === "1*1") {
-		// Business logic for second level response
 		response = "END Your account balance is KES 10,000";
 	} else if (text === "1*2") {
-		// Business logic for second level response
 		response = "END Your account number is ACC1001";
 	} else {
 		response = "END Invalid input";
